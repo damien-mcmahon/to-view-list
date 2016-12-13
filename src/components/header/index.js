fc7df1c27ae93ef1dda50f1fd5ff08adf9038357
Damien McMahon <damien@macoto.co.uk>
2016-12-13 11:28:27 +0000

    Cleaning out the unneeded stuff

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,17 +1,11 @@
 import { h, Component } from 'preact';
 import { Link } from 'preact-router';
-import style from './style';
 
 export default class Header extends Component {
 	render() {
 		return (
-			<header class={style.header}>
-				<h1>Preact Boilerplate</h1>
-				<nav>
-					<Link href="/">Home</Link>
-					<Link href="/profile">Me</Link>
-					<Link href="/profile/john">John</Link>
-				</nav>
+			<header class="toViewApp--header>
+				<h1>To-View</h1>
 			</header>
 		);
 	}
--- src/components/home/index.js
+++ src/components/home/index.js
@@ -4,9 +4,8 @@ import style from './style';
 export default class Home extends Component {
 	render() {
 		return (
-			<div class={style.home}>
-				<h1>Home</h1>
-				<p>This is the Home component.</p>
+			<div>
+       <h1>HOME</h1> 
 			</div>
 		);
 	}
