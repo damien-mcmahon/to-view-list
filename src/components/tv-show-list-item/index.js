85e2cd1faa27ed5050702a705c09509df4df26c7
Damien McMahon <damien@macoto.co.uk>
2016-12-17 16:48:18 +0000

    More styling and the beginings of some of the smarter things

--- src/components/tv-show-list-item/index.js
+++ src/components/tv-show-list-item/index.js
@@ -1,5 +1,11 @@
 import { h, Component } from 'preact';
+import moment from 'moment';
 import TvShowPoster from '../tv-show-poster/';
+
+const STATUS_TO_CSS_MAP = {
+  'ended': '--has-ended',
+  'returning series': '--is-returning'
+};
 
 export default class TvShowListItem extends Component {
   constructor(props) {
@@ -11,22 +17,42 @@ export default class TvShowListItem extends Component {
     this.props.onRemove(this.props.show);
   }
 
+
+  dayOfWeekAired(show) {
+    let airDate = new moment(show.last_air_date);
+
+    console.log("SHOW LAD: ", show.last_air_date);
+
+    return airDate.format('dddd');
+  }
+
   render() {
     const show = this.props.show;
+    const cssStatusClass = STATUS_TO_CSS_MAP[show.status.toLowerCase()];
+    console.log("SHOW:", show);
     return (
-      <div class="tv-show-list-item--wrapper">
+      <div class={`tv-show-list-item--wrapper ${cssStatusClass}`}>
         <TvShowPoster path={show.poster_path} tvShow={show.name} size="medium" />
-        <div class="tv-show-list-item--title--wrapper">
-          <h1>{show.name}</h1>
-        </div>
-        <div class="tv-show-list-item--info-wrapper">
-          <p class="tv-show-list-item--network">{show.networks[0].name}</p>
-          <p class="tv-show-list-item--seasons">{show.number_of_seasons}</p>
-          <p class="tv-show-list-item--episodes">{show.number_of_episodes}</p>
-          <p class="tv-show-list-item--status">{show.status}</p>
-        </div>
-        <div class="tv-show-list-item--actions-wrapper">
-          <button class="button tv-show-list-item--remove" onClick={this.removeButtonClickHandler}>Remove</button>
+        <div class="tv-show-list-item--info-section-wrapper">
+          <div class="tv-show-list-item--title-wrapper">
+            <h1 class="tv-show-list-item--title">
+              <a href={show.homepage} target="_blank">{show.name}</a>
+            </h1>
+            {show.networks.map((network) => {
+              <span class="tv-show-list-item--network-name">{network.name}</span>
+            })}
+            <button class="button tv-show-list-item--info" onClick={this.showInfoPanel}>info</button>
+            <button class="button tv-show-list-item--remove" onClick={this.removeButtonClickHandler}>X</button>
+          </div>
+          <div class="tv-show-list-item--info-wrapper">
+            <span class="tv-show-list-item--seasons">{show.number_of_seasons} seasons</span>
+            <span class="tv-show-list-item--episodes">{show.number_of_episodes} epidodes</span>
+            {show.in_production &&
+              <span class="tv-show-list-item--day-of-week">{this.dayOfWeekAired(show)}</span>
+            }
+          </div>
+          <div class="tv-show-list-item--progress-wrapper">
+          </div>
         </div>
       </div>
     );
