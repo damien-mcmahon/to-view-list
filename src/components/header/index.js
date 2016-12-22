28cca26f70bd7316ad346662507aa8ea59d7a0e1
Damien McMahon <damien@macoto.co.uk>
2016-12-22 19:34:13 +0000

    WIP - Better styling of the basic components and layout

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,11 +1,13 @@
 import { h, Component } from 'preact';
 import { Link } from 'preact-router';
+import TVIcon from '../icon';
 
 export default class Header extends Component {
 	render() {
 		return (
 			<header class="to-view-app--header">
-				<h1>To-View</h1>
+        <TVIcon iconName="tv-logo" class="header--app-logo"/>
+				<h1 class="header--app-title">To-View List</h1>
 			</header>
 		);
 	}
--- src/components/search-result-item/index.js
+++ src/components/search-result-item/index.js
@@ -1,5 +1,6 @@
 import { h, Component } from 'preact';
 import TvShowPoster from '../tv-show-poster/';
+import TVIcon from '../icon';
 
 export default class SearchResultItem extends Component {
   constructor(props) {
@@ -17,7 +18,9 @@ export default class SearchResultItem extends Component {
       <li class="search-result-item--wrapper">
         <TvShowPoster path={result.poster_path} tvShow={result.name} size="small" />
         <h1 class="search-result-item--name">{result.name}</h1>
-        <button class="button --action-add search-result-item--add-to-list" onClick={this.sendItem}>Add to List</button>
+        <button class="button --action-add search-result-item--add-to-list" onClick={this.sendItem}>
+          <TVIcon iconName="plus" />
+        </button>
       </li>
     );
   }
