874b163680de9f7f5af7b6839b9e4dcbceda7bb9
Damien McMahon <damien@macoto.co.uk>
2016-12-22 21:02:54 +0000

    WIP - Paddings normalised, search results position corrected

--- src/containers/home/index.js
+++ src/containers/home/index.js
@@ -78,8 +78,10 @@ class Home extends Component {
     
 		return (
 			<div class="to-view-list--home--wrapper">
-        <TvSearch searchFor={this.doSearch} clearSearch={this.state.clearSearch}/>
-        <SearchResultList resultsSet={this.state.searchResults} onSelect={this.selectShow} />
+        <div class="home-view--search-wrapper">
+          <TvSearch searchFor={this.doSearch} clearSearch={this.state.clearSearch}/>
+          <SearchResultList resultsSet={this.state.searchResults} onSelect={this.selectShow} />
+        </div>
         <TvShowList shows={shows} removeItem={this.removeItem} showInfoPanel={this.showInfoPanel}/>
         {this.props.showPanel.visible &&
           <TvShowPanel />
