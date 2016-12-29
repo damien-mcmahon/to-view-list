ec47a3eb961710779a61871546d4bf585924e2e1
Damien McMahon <damien@macoto.co.uk>
2016-12-29 16:52:50 +0000

    Seasons episodes lists are looking better

--- src/components/episode-item/index.js
+++ src/components/episode-item/index.js
@@ -1,4 +1,7 @@
 import { h, Component } from 'preact';
+import moment from 'moment';
+
+const AIR_DATE_FORMAT = 'MMM Do YYYY';
 
 export default class EpisodeItem extends Component {
   constructor(props) {
@@ -13,12 +16,19 @@ export default class EpisodeItem extends Component {
 
   render() {
     const { episode, hasWatched, seasonComplete } = this.props;
-
+    const airDate = new moment(episode.air_date).format(AIR_DATE_FORMAT);
     return (
       <li class="episode-item--wrapper">
         <label class="episode-item--label">
-          <input type="checkbox" onClick={this.sendWatched} checked={hasWatched} disabled={seasonComplete} /> {episode.name}
+          <input 
+            type="checkbox" 
+            onClick={this.sendWatched} 
+            checked={hasWatched} 
+            disabled={seasonComplete} /> 
+          <span class="episode-item--number">{episode.episode_number} </span>-  
+          <span class="episode-item--name"> {episode.name}</span>
         </label>
+        <span class="episode-item--air-date">{airDate}</span>
       </li>
     );
   }
