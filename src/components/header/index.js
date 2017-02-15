d7d7fb112d17deafedda51edd0e40bf80cc57c53
Damien <damien.mcmahon@gmail.com>
2017-02-15 18:28:58 +0000

    WIP - Adds home menu and some link styling. Header nav will be a stateful component after all the messing

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -10,6 +10,7 @@ export default class Header extends Component {
         <TVIcon iconName="tv-logo" class="header--app-logo"/>
 				<h1 class="header--app-title">To-View List</h1>
         <HeaderNavigation>
+          <a href="/">Home</a>
           <a href="/about">About</a>
         </HeaderNavigation>
 			</header>
