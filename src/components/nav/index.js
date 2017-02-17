7dc8b6ebe79cc95ac94b692f69dbe96b3bfbdece
Damien <damien.mcmahon@gmail.com>
2017-02-17 18:18:48 +0000

    Side nav works on mobile nicely

--- src/components/nav/index.js
+++ src/components/nav/index.js
@@ -5,6 +5,7 @@ const HeaderNavigation = props => (
   <nav class="nav--wrapper">
     <input type="checkbox" id="nav--menu-input" />
     <label class="nav--menu-button button" for="nav--menu-input">
+      <span class="nav--menu-button-label">NAV</span>
     </label>
     <div class="nav--menu-wrapper">
       {props.children}
