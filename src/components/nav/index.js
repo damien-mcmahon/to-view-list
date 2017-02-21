a0d7a06b436a084d98c70f35e0840bbbb1f14868
Damien <damien.mcmahon@gmail.com>
2017-02-21 18:38:56 +0000

    Adds some pure nav handling (refs the DOM but hey ho 🤷

--- src/components/nav/index.js
+++ src/components/nav/index.js
@@ -1,16 +1,28 @@
 import { h, Component } from 'preact';
-import { pure } from 'recompose';
+import { pure, compose, withHandlers} from 'recompose';
 
-const HeaderNavigation = props => (
+const enhanced = compose(
+  withHandlers({
+   onClick: props => event => {
+     const navCheckBox = document.getElementById('nav--menu-input');
+     
+     if (navCheckBox) {
+       navCheckBox.checked = !navCheckBox.checked;
+     }
+   }
+  }),
+  pure
+);
+const HeaderNavigation = (props) => (
   <nav class="nav--wrapper">
     <input type="checkbox" id="nav--menu-input" />
     <label class="nav--menu-button button" for="nav--menu-input">
       <span class="nav--menu-button-label">NAV</span>
     </label>
-    <div class="nav--menu-wrapper">
+    <div class="nav--menu-wrapper" onClick={props.onClick}>
       {props.children}
     </div>
   </nav>
 );
 
-export default pure(HeaderNavigation);
+export default enhanced(HeaderNavigation);
