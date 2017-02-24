d47c4edcc0628d6c9e55e923caefad6f57baf278
Damien <damien.mcmahon@gmail.com>
2017-02-24 17:30:53 +0000

    More text on the about page

--- src/containers/about/index.js
+++ src/containers/about/index.js
@@ -5,12 +5,12 @@ export default class About extends Component {
     return (
       <div class="to-view-list--about-wrapper">
         <h1 class="about--title">About</h1>
-        <p class="about--text">A simple To-do list app but for TV Shows.</p>
+        <p class="about--text">A simple To-do list app but for TV Shows. Drill down into each show to find out more about each season, ticking off episodes as you watch them.</p>
         <h2 class="about--technology">All the Tech</h2>
-        <p class="about--text">Built using <a href="https://preactjs.com">Preact</a>, <a href="https://reduxjs.org">Redux</a> running on <a href="https://surge.sh">Surge</a>
+        <p class="about--text">Built using <a href="https://preactjs.com">Preact</a>, <a href="https://reduxjs.org">Redux</a> running on <a href="https://surge.sh">Surge</a> with data provided by <a href="http://themoviedb.org">The Movie Database</a>
         </p>
-        <h3 class="about--creator">Built with ❤️ </h3>
-        <p class="about--text">Built by <a href="http://macoto.co.uk">Macoto</a> for my wife who watches too much Tv.</p>
+        <h3>All the Devs</h3>
+        <p class="about--text">Built by: <a href="http://macoto.co.uk">Macoto Ltd</a></p>
       </div>
     );
   }
